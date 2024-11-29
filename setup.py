b9915c56685f5d44dbddc17c03e2b46a1a31db5c
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2024-11-29 17:20:08 +0100

    Add CICD support

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
+import re
+import os
 
-setup(
-    name="csra",
-    version="0.1.0",
-    packages=["sra_collector"],
-    entry_points={
-        "console_scripts": [
-            "csra=sra_collector.cli:main",
-        ]
-    },
-)
+MODULE_NAME = "sra_collector"
+
+def get_version():
+    version_file =  os.path.join(os.path.dirname(__file__), MODULE_NAME, "__init__.py")
+    with open(version_file, "r") as f:
+        content = f.read()
+        version_match = re.search(r'^__version__\s*=\s*["\']([^"\']+)["\']', content, re.M)
+        if version_match:
+            return version_match.group(1)
+        raise RuntimeError("Unable to find version string.")
+
+setup(name="csra", version=get_version(), packages=[MODULE_NAME], entry_points={"console_scripts": [f"csra={MODULE_NAME}.cli:main", ]}, )
--- sra_collector/cli.py
+++ sra_collector/cli.py
@@ -1,29 +1,18 @@
 import argparse
 import sys
-import re
-
-
-def get_version_from_setup():
-    with open("setup.py", "r") as file:
-        content = file.read()
-        match = re.search(r"version=['\"]([^'\"]+)['\"]", content)
-        if match:
-            return match.group(1)
-        else:
-            raise ValueError("Version not found in setup.py")
-
+from sra_collector import __version__
 
 def main():
     parser = argparse.ArgumentParser(description="Collect NIH NCBI SRA metadata of several GEO studies in one search")
 
-    parser.add_argument("-v", "--version", action="version", version=f"%(prog)s version {get_version_from_setup()}")
+    parser.add_argument("-v", "--version", action="version", version=f"%(prog)s version {__version__}")
     parser.add_argument("-q", "--query", nargs="+", type=str, help="The NIH NCBI query between quotes", required=True)
     args = parser.parse_args()
 
     if args.query:
         if len(args.query) > 1:
-            print("Error: It looks like your query isn't quoted.")
-            print("Usage: csra \"your query here\"")
+            print("Error: It looks like your query isn't quoted.",file=sys.stderr)
+            print("Usage: csra \"your query here\"",file=sys.stderr)
             sys.exit(1)
         else:
             query = " ".join(args.query)
