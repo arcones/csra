6dde2f68f5e6b7c06cf58eeba86986726d47bc46
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2024-11-29 18:50:15 +0100

    WIP

--- setup.py
+++ setup.py
@@ -1,23 +1,12 @@
 from setuptools import setup
-import re
 from pathlib import Path
 
 MODULE_NAME = "sra_collector"
 README_CONTENT = (Path(__file__).parent / "README.md").read_text()
 
-
-def get_version():
-    version_file = Path(__file__).parent / MODULE_NAME / "__init__.py"
-    with open(version_file, "r") as f:
-        content = f.read()
-        version_match = re.search(r'^__version__\s*=\s*["\']([^"\']+)["\']', content, re.M)
-        if version_match:
-            return version_match.group(1)
-        raise RuntimeError("Unable to find version string.")
-
-
 setup(name="csra",
-      version=get_version(),
+      version="0.0.1",
+      description="Collect NIH NCBI SRA metadata of several GEO studies in one search.",
       packages=[MODULE_NAME],
       long_description=README_CONTENT,
       long_description_content_type="text/markdown",
