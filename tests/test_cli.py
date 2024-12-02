75bac35398f578a48ffb3d190098f8f91505b4e5
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2024-12-02 21:09:37 +0100

    Try poetry

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,36 +1,34 @@
-import unittest
 import subprocess
-
-
-class TestCSRA(unittest.TestCase):
-
-    def test_success_single_word_query(self):
-        single_word_query = "rheumatism"
-        result = run_cli("--query", single_word_query)
-        self.assertEqual(0, result.returncode)
-        self.assertIn(f"Received query: {single_word_query}", result.stdout)
-
-    def test_success_multiword_query(self):
-        multiword_query = '"multiple sclerosis"'
-        result = run_cli("--query", multiword_query)
-        self.assertEqual(0, result.returncode)
-        self.assertIn(f"Received query: {multiword_query}", result.stdout)
-
-    def test_fail_multiword_query_not_quoted(self):
-        result = run_cli("--query", "liver", "cirrhosis")
-        self.assertEqual(1, result.returncode)
-        self.assertIn("Error: It looks like your query isn\'t quoted.\nUsage: csra \"your query here\"", result.stderr)
-
-    def test_version(self):
-        result = run_cli("--version")
-        self.assertEqual(0, result.returncode)
-        self.assertIn("csra version 0.0.9", result.stdout)
+import pytest
 
 
 def run_cli(*args):
+    """Helper function to run the csra CLI and return the result."""
     result = subprocess.run(["csra"] + list(args), capture_output=True, text=True)
     return result
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_success_single_word_query():
+    single_word_query = "rheumatism"
+    result = run_cli("--query", single_word_query)
+    assert result.returncode == 0
+    assert f"Received query: {single_word_query}" in result.stdout
+
+
+def test_success_multiword_query():
+    multiword_query = '"multiple sclerosis"'
+    result = run_cli("--query", multiword_query)
+    assert result.returncode == 0
+    assert f"Received query: {multiword_query}" in result.stdout
+
+
+def test_fail_multiword_query_not_quoted():
+    result = run_cli("--query", "liver", "cirrhosis")
+    assert result.returncode == 1
+    assert "Error: It looks like your query isn't quoted.\nUsage: csra \"your query here\"" in result.stderr
+
+
+def test_version():
+    result = run_cli("--version")
+    assert result.returncode == 0
+    assert "csra version 0.0.9" in result.stdout
